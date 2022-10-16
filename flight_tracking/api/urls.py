0190c7fc7e96475a5ec45f9f4d416ff97460f153
phuoc tran <phuoc.tran@parcelperform.com>
2022-10-16 18:14:45 +0700

    update api

--- flight_tracking/api/urls.py
+++ flight_tracking/api/urls.py
@@ -4,7 +4,7 @@ from .views.add_new_flight import add_new_flight
 from .views.add_airport import add_airport
 
 urlpatterns = [
-    path("flights/<str:flight_id>/", get_flight_info, name="get_flight_info"),
+    path("flights/<str:flight_number>/", get_flight_info, name="get_flight_info"),
     path("flights/", add_new_flight, name="add_new_flight"),
     path("airports/", add_airport, name="add_airport"),
 ]
--- flight_tracking/api/views/get_flight_info.py
+++ flight_tracking/api/views/get_flight_info.py
@@ -1,14 +1,15 @@
 from ..models.flight import Flight
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @api_view(["GET"])
-def get_flight_info(request, flight_id):
-    """Get flight info by flight id
+def get_flight_info(request, flight_number):
+    """Get flight info by flight number
     ---
     parameters:
-        - flight_id: string
+        - flight_number: string
     responseMessages:
         - code: 200
           message: Success
@@ -16,7 +17,17 @@ def get_flight_info(request, flight_id):
           message: Not found
     """
     try:
-        flight = Flight.objects.get(id=flight_id)
-        return Response(flight.to_dict(), status=status.HTTP_200_OK)
+        flight = Flight.objects.get(flight_number=flight_number)
+        return Response(
+            {
+                "flight_number": flight.flight_number,
+                "departure_airport": flight.departure_airport,
+                "arrival_airport": flight.arrival_airport,
+                "departure_time": flight.departure_time,
+                "arrival_time": flight.arrival_time,
+                "status": flight.status,
+            },
+            status=status.HTTP_200_OK,
+        )
     except Flight.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
